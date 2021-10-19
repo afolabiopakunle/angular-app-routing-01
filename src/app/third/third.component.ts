dbe5cf60eb50a4897ccb24924507c0b7f9ca6f6f
afolabiopakunle <opakunleafolabi@gmail.com>
2021-10-19 08:59:03 +0100

    collapsed

---
+++ src/app/third/third.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-third',
+  styleUrls: ['./third.component.css'],
+  templateUrl: './third.component.html'
+})
+
+export class ThirdComponent implements OnInit {
+
+  ngOnInit() {
+    
+  }
+}
